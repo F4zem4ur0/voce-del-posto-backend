ee48cb40481e092b72a2ed702266b91cc7d21831
MALO <dynamicmalox@gmail.com>
2026-05-15 11:45:38 +0200

    fix: prepareThreshold=0 per Supabase pooler + test raccomandazioni

--- src/main/java/com/vocedelposto/backend/controller/AppUserController.java
+++ src/main/java/com/vocedelposto/backend/controller/AppUserController.java
@@ -5,6 +5,8 @@ import com.vocedelposto.backend.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import com.vocedelposto.backend.model.Tag;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/users")
@@ -20,6 +22,12 @@ public class AppUserController {
 
     @PostMapping
     public AppUser createUser(@RequestBody AppUser user) {
+        return appUserRepository.save(user);
+    }
+    @PutMapping("/{id}/tags")
+    public AppUser updateTags(@PathVariable Long id, @RequestBody Set<Tag> tags) {
+        AppUser user = appUserRepository.findById(id).orElseThrow();
+        user.setPreferredTags(tags);
         return appUserRepository.save(user);
     }
 }
